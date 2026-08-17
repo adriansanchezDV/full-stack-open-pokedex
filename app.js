f877a2401142af897165e20244d3bf956cd07f55
Adrian Sanchez Huertas <adriansanchezhuertas26@gmail.com>
2026-08-17 20:35:01 +0200

    Test health check failure

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get("/health", (req, res) => {
-  res.status(500).send("error")
+app.get('/health', (req, res) => {
+  res.status(500).send('error')
 })
 
 app.listen(PORT, () => {
